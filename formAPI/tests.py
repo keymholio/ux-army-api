6429bb59b49801f983c8697af248c7cfecf1e394
Victor Oza <victorozadev@gmail.com>
2014-08-08 08:58:28 -0400

    fixed failing text

--- formAPI/tests.py
+++ formAPI/tests.py
@@ -114,7 +114,7 @@ class ParticipantListTest(APITestCase):
                     'password' : 'incorrect_pass'
                 }
             )
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_login_token(self):
         response = self.client.post('/login/', 
